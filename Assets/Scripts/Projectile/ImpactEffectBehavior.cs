248349b6861e42003b388046db4710734d594508
Isaac Denny <isaac_denny@gmail.com>
2022-10-24 15:47:58 -0600

    Fixed Some Script Naming Issues and Cleaned out unused bullet script

--- Assets/Scripts/Projectile/ImpactEffectBehavior.cs
+++ Assets/Scripts/Projectile/ImpactEffectBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImpactEffectBehaviour : MonoBehaviour
+public class ImpactEffectBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
